86c023b91a37dce052f968c5086cdb7a5d746bc1
Kerekes Denisa <denisa.camelia0717@gmail.com>
2022-08-03 11:52:58 +0300

    Add project files.

---
+++ Selenium_WebDriver/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Selenium {
+    public static class Selenium
+    {
+        public static void Main(string[] args)
+        {
+            IWebDriver webDriver = StartChromeDriver();
+
+
+
+        }
+        public static IWebDriver StartChromeDriver()
+        {
+
+            IWebDriver WebDriver = new ChromeDriver("C://Users//denis//OneDrive//Desktop//internshipEvozon//Selenium_WebDriver//Selenium_WebDriver//Drivers");
+
+            WebDriver.Navigate().GoToUrl("http://qa1magento.dev.evozon.com");
+
+            return WebDriver;
+            
+        }
+        public static void FindTitle(IWebDriver WebDriver)
+        {
+            IWebElement getTitle = WebDriver.FindElement(By.CssSelector("#header > div > a > img.large"));
+        }
+
+    }
+}
