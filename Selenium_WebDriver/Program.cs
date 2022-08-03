d0c4f2b87fc36ab7ca4d01e0a2a41b664c14e2eb
Kerekes Denisa <denisa.camelia0717@gmail.com>
2022-08-03 15:03:46 +0300

    1

--- Selenium_WebDriver/Program.cs
+++ Selenium_WebDriver/Program.cs
@@ -3,29 +3,88 @@ using OpenQA.Selenium.Chrome;
 
 
 namespace Selenium {
-    public static class Selenium
+    public class Selenium
     {
+        IWebDriver webDriver;
         public static void Main(string[] args)
         {
-            IWebDriver webDriver = StartChromeDriver();
+            Selenium s = new Selenium();
 
-
+            s.StartChromeDriver();
+            s.FindTitle();
+            s.FindURL();
+            s.FindLogo();
+            s.ClickLogo();
+            s.NavigateToPage("WOMEN");
+            s.NavigateBack();
+            s.NavigateForward();
+            s.Refresh();  
+            s.Quit();
+            s.StartChromeDriver();
+            s.ClickAccount();
+            s.Quit(); 
 
         }
-        public static IWebDriver StartChromeDriver()
+        public void StartChromeDriver()
         {
+            webDriver = new ChromeDriver("C://Users//denis//OneDrive//Desktop//internshipEvozon//Selenium_WebDriver//Selenium_WebDriver//Drivers");
 
-            IWebDriver WebDriver = new ChromeDriver("C://Users//denis//OneDrive//Desktop//internshipEvozon//Selenium_WebDriver//Selenium_WebDriver//Drivers");
+            webDriver.Navigate().GoToUrl("http://qa1magento.dev.evozon.com");
 
-            WebDriver.Navigate().GoToUrl("http://qa1magento.dev.evozon.com");
-
-            return WebDriver;
-            
         }
-        public static void FindTitle(IWebDriver WebDriver)
+        public void FindTitle()
         {
-            IWebElement getTitle = WebDriver.FindElement(By.CssSelector("#header > div > a > img.large"));
+            Console.WriteLine("Title is: " + webDriver.Title);
         }
 
+        public void FindURL()
+        {
+            string url = webDriver.Url;
+            Console.WriteLine("URL este: " + url);
+        }
+        public void FindLogo()
+        {
+            IWebElement getLogo = webDriver.FindElement(By.CssSelector("#header > div > a > img.large"));
+            Console.WriteLine("Logo is: " + getLogo);
+        }
+        public void ClickLogo()
+        {
+            IWebElement clickLogo = webDriver.FindElement(By.CssSelector("#header > div > a > img.large"));
+            clickLogo.Click();
+        }
+        public void NavigateToPage(string name)
+        {
+            IList<IWebElement> productList = webDriver.FindElements(By.CssSelector("#nav > ol > li"));
+            foreach (var i in productList) 
+            {
+                if (i.Text.Equals(name))
+                {
+                    Console.WriteLine(i);
+                    i.Click();
+                    break;
+                }
+            }
+        }
+        public void NavigateBack()
+        {
+            webDriver.Navigate().Back();
+        }
+        public void NavigateForward()
+        {
+            webDriver.Navigate().Forward();
+        }
+        public void Refresh()
+        {
+            webDriver.Navigate().Refresh();
+        }
+        public void Quit()
+        {
+            webDriver.Quit();
+        }
+        public void ClickAccount()
+        {
+            IWebElement clickLogo = webDriver.FindElement(By.CssSelector("#header > div > div.skip-links > div > a > span.label"));
+            clickLogo.Click();
+        }
     }
 }
